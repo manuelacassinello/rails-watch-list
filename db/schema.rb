fa1c8fcaf4cffa3fb18eae787baa9df34d46c93f
Manuela Rodriguez-Acosta Cassinello <mrac.in@icloud.com>
2021-08-19 12:13:54 +0100

    model list

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_110404) do
+ActiveRecord::Schema.define(version: 2021_08_19_111307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2021_08_19_110404) do
     t.string "comment"
     t.integer "movie_id"
     t.integer "list_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
